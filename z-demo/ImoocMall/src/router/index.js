cb7a5fdc69966f766c130006f961f197cd643b1c
bentiancai629 <bentiancai629@gmail.com>
2019-11-30 14:30:36 +0800

    动态路由

--- z-demo/ImoocMall/src/router/index.js
+++ z-demo/ImoocMall/src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '../views/GoodsList.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  //选择路由模式
+  // model:'history',
+
   routes: [
+    // {
+      // path: '/',
+      // name: 'HelloWorld',
+      // component: HelloWorld
+    // }
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      // path: '/goods/:goodsId',
+      path: '/goods/:goodsId/user/:name',
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
---
+++ z-demo/ImoocMall/src/views/GoodsList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        商品列表
+        <!-- 动态路由 router传递goodsId -->
+        <!-- http://localhost:8080/#/goods/88 -->
+        <span>{{ $route.params.goodsId }}</span>
+        <br>
+        <!-- '/goods/:goodsId/user/:name', -->
+         <span>{{ $route.params.name }}</span>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from 'vue'
+import OtherComponent from 'vue'
+
+export default ({
+    data (){
+        return {
+            msg :'hi vue'
+        }
+    }
+})
+</script>
