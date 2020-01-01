4e222fae6167bd2deb85e5a8b07b3f0a2dc76ffe
bentiancai629 <bentiancai629@gmail.com>
2020-01-01 11:05:12 +0800

    16-8 多页面打包

--- demo/webpack-demo/dist/js/2.js
+++ demo/webpack-demo/dist/js/2.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{2:function(n,o,i){var c;void 0===(c=function(){return{initIndex:function(){console.log("common init index")},initCart:function(){console.log("common init cart")}}}.call(o,i,o,n))||(n.exports=c)}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{3:function(n,o,i){var c;void 0===(c=function(){return{initIndex:function(){console.log("common init index")},initCart:function(){console.log("common init cart")}}}.call(o,i,o,n))||(n.exports=c)}}]);
--- demo/webpack-demo/webpack.config.js
+++ demo/webpack-demo/webpack.config.js
@@ -1,6 +1,11 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 // cnpm i --save-dev html-webpack-plugin 
+// cnpm i --save-dev clean-webpack-plugin
+// cnpm i --save-dev css-loader style-loader mini-css-extract-plugin 
 module.exports = {
     entry:{
         index:'./src/js/index.js',
@@ -9,19 +14,42 @@ module.exports = {
     output:{
         path:path.join(__dirname,'./dist'),
         filename:'js/[name].js',
-        publicPath:'http://cloud.baidu.com'
+        publicPath:''
     },
     module:{
+        rules:[
+            {
+                test:/\.css$/,
+                include:path.join(__dirname,'src'),
+                exclude:/node_modules/,
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {
+                        // you can specify a publicPath here
+                        // by default it uses publicPath in webpackOptions.output
+                        publicPath: '../',
+                        hmr: process.env.NODE_ENV === 'development',
+                      },
+                    },
+                    'css-loader',
+                  ],
 
+            }
+        ]
     },
     plugins:[
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename:'index.html',
-            template:'./src/index.html'
+            template:'./src/index.html',
+            chunks:['index']
         }),
         new HtmlWebpackPlugin({
             filename:'cart.html',
-            template:'./src/cart.html'
+            template:'./src/cart.html',
+            chunks:['cart']
         }),
     ],
     // devtool:'#source-map'
